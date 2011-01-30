1da3ee1eb1faa2fcb710302364ce1ce7efd1936c
Rich Leland <rich@richleland.com>
2011-01-30 12:28:02 -0500

    prep setup for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='typogrify',
-      version='1.0',
-      description='Typography related template filters for Django applications',
-      author='Christian Metts',
-      author_email='xian@mintchaos.com',
-      url='http://code.google.com/p/typogrify/',
-      packages=['typogrify', 'typogrify.templatetags'],
-      classifiers=['Development Status :: 1',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers :: Designers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='typogrify',
+    version='1.0',
+    packages=find_packages(),
+
+    author='Christian Metts',
+    author_email='xian@mintchaos.com',
+    license='BSD',
+    url='https://github.com/mintchaos/typogrify',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers :: Designers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities'
+    ]
+)
